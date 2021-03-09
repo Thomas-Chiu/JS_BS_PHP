bd4da9b45ebffdfd6e6ae9232cc4b4c0959bb46c
Thomas-Chiu <josemich0511@gmail.com>
2021-03-09 23:16:19 +0800

    revise objects.php

--- S3. PHP/93. staticKeyword.php
+++ S3. PHP/93. staticKeyword.php
@@ -1,36 +1,13 @@
 <?php
-// 建立類別
-class Motorbike {
-  // 定義屬性
-  public $brand;
-  private $model;
-  protected $style;
-
-  // 建構物件中屬性值的函式 (把參數傳進物件中的變數)
-  public function __construct($sourceBrand, $sourceModel, $sourceStyle) {
-    // $this (這個物件) -> (裡面的) brand (變數) = $sourceBrand (建立新物件傳入的參數)
-    // 因為物件內不能直接使用物件外傳入的參數，內外不共通，一定要透過 -> 語法才能變更
-    $this->brand = $sourceBrand; 
-    $this->model = $sourceModel;
-    $this->style = $sourceStyle;
-  }
-
-  // 定義方法
-  public function showMotorbike() {
-    return "我的摩托車是 " . $this->brand . " 的 " . $this->model . "&nbsp" . $this->style;
-  } 
-
-  // 解構 (腳本結束時) 物件的函式
-  function __destruct()  {
-    echo "<hr>" . $this->brand . " 解構中......";
-  }
+function test1() {
+  static $num = 10; // static 關鍵字保留區域變數 $num
+  echo $num;
+  $num++;
 }
 
-// 建立新物件
-$myMotorbike = new Motorbike("YAMAHA", "XSR700", "Neo-retro");
-$myMotorbike2 = new Motorbike("HONDA", "CB350", "Scrambler");
-
-// 印出新物件的資料
-echo $myMotorbike->showMotorbike() . "<br>"; 
-echo $myMotorbike2->showMotorbike(); 
+for ($i=0; $i<=10; $i++) {
+  // 印出 $num 10～20
+  test1();
+  echo "<br>";
+}
 ?>
--- S3. PHP/96. objects.php
+++ S3. PHP/96. objects.php
@@ -1,9 +1,36 @@
 <?php
-$array = [1,2,3];
-var_dump($array);
+// 建立類別
+class Motorbike {
+  // 定義屬性
+  public $brand;
+  private $model;
+  protected $style;
 
-echo "<hr>";
+  // 建構物件中屬性值的函式 (把參數傳進物件中的變數)
+  public function __construct($sourceBrand, $sourceModel, $sourceStyle) {
+    // $this (這個物件) -> (裡面的) brand (變數) = $sourceBrand (建立新物件傳入的參數)
+    // 因為物件內不能直接使用物件外傳入的參數，內外不共通，一定要透過 -> 語法才能變更
+    $this->brand = $sourceBrand; 
+    $this->model = $sourceModel;
+    $this->style = $sourceStyle;
+  }
 
-$array2 = array (1, 2, 3);
-var_dump($array2);
+  // 定義方法
+  public function showMotorbike() {
+    return "我的摩托車是 " . $this->brand . " 的 " . $this->model . "&nbsp" . $this->style;
+  } 
+
+  // 解構 (腳本結束時) 物件的函式
+  function __destruct()  {
+    echo "<hr>" . $this->brand . " 解構中......";
+  }
+}
+
+// 建立新物件
+$myMotorbike = new Motorbike("YAMAHA", "XSR700", "Neo-retro");
+$myMotorbike2 = new Motorbike("HONDA", "CB350", "Scrambler");
+
+// 印出新物件的資料
+echo $myMotorbike->showMotorbike() . "<br>"; 
+echo $myMotorbike2->showMotorbike(); 
 ?>
